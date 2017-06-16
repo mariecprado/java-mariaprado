d07125cad4a6555d487c6d30a20a9ecae79703bc
mariecprado <mariadelcarmen.prado22@galileo.edu>
2017-06-16 09:23:08 -0600

    sphere.java

--- Sphere.java
+++ Sphere.java
@@ -2,7 +2,20 @@ public class Sphere extends Circle{
 	public double calculateAreaSphere(){
 		return 4 * 25.55 * 25.55 * 3.14159;
 	}
+	
+	public double calculateDiameterS(){
+		return calculateDiameter();
+	}
+	
+	public double getRadiusS(){
+		return getRadius();
+	}
 
+	public double calculateCircumferenceS(){
+		return calculateDiameter();
+	 }
+	
+	
 
 	public static void main(String[] args){
 	 	Sphere esfera = new Sphere();
@@ -13,31 +26,9 @@ public class Sphere extends Circle{
 	 	System.out.println("Circumference:"+ circulo.calculateCircumference());
 	 	System.out.println("Area:" + circulo.calculateArea());
 	 	System.out.println("Sphere Characteristics");
-	 	System.out.println("Side:"+ esfera.getRadius());
-	 	System.out.println("Diameter:"+ esfera.calculateDiameter());
-	 	System.out.println("Circumference:"+ esfera.calculateCircumference());
+	 	System.out.println("Side:"+ esfera.getRadiusS());
+	 	System.out.println("Diameter:"+ esfera.calculateDiameterS());
+	 	System.out.println("Circumference:"+ esfera.calculateCircumferenceS());
 	 	System.out.println("Area:"+ esfera.calculateAreaSphere());
-	}
-
-static class Circle{
-		public double radius = 25.55;
-
-		public double getRadius(){
-		
-			return radius;
-		}
-	 	
-	 	public double calculateDiameter(){
-	 		double diameter = radius * 2;
-	 		return diameter;
-	 	}
-
-	 	public double calculateCircumference(){
-	 		return calculateDiameter() * 3.14159;
-	 	}
-
-	 	public double calculateArea(){
-	 		return radius * radius * 3.14159;
-	 	}
 	}
 }
